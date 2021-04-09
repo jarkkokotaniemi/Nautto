96f3421e5b0de3a0324f86d5999cd2ecebc6acf8
vtiinanen <vili.tiinanen@live.fi>
2021-04-09 10:54:40 +0300

    Add set resource feature

--- nautto/api.py
+++ nautto/api.py
@@ -2,6 +2,7 @@
 from nautto.resources.user import UserCollection, UserItem
 from nautto.resources.widget import WidgetsByUserCollection, WidgetCollection, WidgetItem
 from nautto.resources.layout import LayoutsByUserCollection, LayoutCollection, LayoutItem
+from nautto.resources.set import SetsByUserCollection, SetCollection, SetItem
 from flask import Blueprint
 from flask_restful import Api
 
@@ -18,3 +19,7 @@ api.add_resource(WidgetItem, "/widgets/<widget>/")
 api.add_resource(LayoutsByUserCollection, "/users/<user>/layouts/")
 api.add_resource(LayoutCollection, "/layouts/")
 api.add_resource(LayoutItem, "/layouts/<layout>/")
+
+api.add_resource(SetsByUserCollection, "/users/<user>/sets/")
+api.add_resource(SetCollection, "/sets/")
+api.add_resource(SetItem, "/sets/<set>/")
