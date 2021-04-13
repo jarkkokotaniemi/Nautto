0f6675380a1edc22cd7e3d778a69ae0fcdd31f5f
Jarkko Kotaniemi <jarkko.kotaniemi@vtt.fi>
2021-04-13 10:10:20 +0300

    x-by link refers to the correct place

--- nautto/resources/layout.py
+++ nautto/resources/layout.py
@@ -18,6 +18,7 @@ class LayoutsByUserCollection(Resource):
         body.add_namespace("nautto", LINK_RELATIONS_URL)
         body.add_control("self", url_for("api.layoutsbyusercollection", user=user))
         body.add_control_add_resource('layout', url_for("api.layoutsbyusercollection", user=user))
+        body.add_control("author", url_for("api.useritem", user=user))
         body.add_control("nautto:layouts-all", url_for("api.layoutcollection"))
         body["items"] = []
         for db_layout in Layout.query.filter_by(user_id=user):
@@ -49,9 +50,11 @@ class LayoutsByUserCollection(Resource):
 
         layout = Layout(
             name=request.json["name"],
-            description=request.json["description"],
             user=db_user
         )
+
+        if ('description' in request.json):
+            layout.description = request.json["description"]
 
         if ('id' in request.json):
             layout.id = request.json["id"]
@@ -176,3 +179,30 @@ class LayoutItem(Resource):
         db.session.commit()
 
         return Response(status=204)
+
+class LayoutOfSet(Resource):
+
+    def get(self,set,layout):
+        db_layout = Layout.query.filter_by(id=layout).first()
+        if db_layout is None:
+            return create_error_response(
+                404, "Not found",
+                f'No layout was found with the id {layout}'
+            )
+
+        body = NauttoBuilder(
+            id=db_layout.id,
+            name=db_layout.name,
+            description=db_layout.description,
+        )
+        url_for_item = url_for('api.layoutitem', layout=layout)
+        body.add_namespace("nautto", LINK_RELATIONS_URL)
+        body.add_control("self", url_for_item)
+        body.add_control("profile", LAYOUT_PROFILE)
+        body.add_control("collection", url_for("api.layoutcollection"))
+        body.add_control("author", url_for("api.useritem", user=db_layout.user_id))
+        body.add_control_delete_resource('layout', url_for_item)
+        body.add_control_modify_resource('layout', url_for_item)
+        body.add_control('up', url_for("api.setitem", set=set))
+
+        return Response(json.dumps(body), 200, mimetype=MASON)
